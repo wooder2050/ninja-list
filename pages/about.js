7b611becd64999df161833fe88ca584eb52a4996
wooder2050 <wooder2050@gmail.com>
2021-02-11 22:39:52 +0900

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Cupidatat eu dolor non sunt qui id nulla commodo fugiat nostrud.</p>
+        <p>Labore eu aliquip in amet qui ea nostrud.</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
